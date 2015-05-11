59e37244036a568cb52e64ce0a2b66f8d3c48354
diogomartins <diogo.martins@unirio.br>
2015-05-11 16:30:07 -0300

    Adicionado testes para senhas comuns e de datas

--- src/pwd/CheckStrengthTest.java
+++ src/pwd/CheckStrengthTest.java
@@ -1,6 +1,5 @@
 package pwd;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -9,9 +8,12 @@ import static org.junit.Assert.*;
 
 public class CheckStrengthTest {
     final String[] NULL_PASSWORDS = {null, "", "      "};
-    final String[] EASY_PASSWORDS = {"123456", "abcdefg", "1", "b"};
-    final String[] MEDIUM_PASSWORDS = {"123456abc", "abcdefg123"};
+    final String[] EASY_PASSWORDS = {"aaaaaaaa", "1111111", "123456", "abcdefg", "1", "b"};
+    final String[] MEDIUM_PASSWORDS = {"12345#6aDc", "ABFDcdef$g123"};
     final String[] STRONG_PASSWORDS = {"M38SDg9cbVcdasg", "9aADG(b90d90s0*$8GDSv"};
+    final String[] VERY_STRONG_PASSWORDS = {"Minha m@e G0sta De MiM!!!!"};
+    final String[] DATE_PASSWORDS = {"19871207", "871207"};
+    final String[] DICTIONARY_PASSWORDS = {"password", "abc123", "iloveyou"};
 
 
     @Rule
@@ -20,8 +22,7 @@ public class CheckStrengthTest {
     @Test
     public void testNullPassword() throws Exception{
         for(String password: NULL_PASSWORDS) {
-            thrown.expect(Exception.class);
-            thrown.expectMessage("password is empty");
+            thrown.expect(IllegalArgumentException.class);
             CheckStrength.getPasswordLevel(password);
         }
     }
@@ -30,6 +31,20 @@ public class CheckStrengthTest {
     public void testEasyPasswords(){
         for(String password: EASY_PASSWORDS){
             assertEquals(CheckStrength.LEVEL.EASY,CheckStrength.getPasswordLevel(password));
+        }
+    }
+
+    @Test
+    public void testDatePasswords(){
+        for(String password: DATE_PASSWORDS) {
+            assertEquals(0, CheckStrength.checkPasswordStrength(password));
+        }
+    }
+
+    @Test
+    public void testDictionaryPasswords(){
+        for(String password: DICTIONARY_PASSWORDS) {
+            assertEquals(CheckStrength.LEVEL.EASY, CheckStrength.getPasswordLevel(password));
         }
     }
 }
