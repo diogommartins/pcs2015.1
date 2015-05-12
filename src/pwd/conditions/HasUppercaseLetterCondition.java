4f73648ada9ae533e2aeff36a989f10b0a0a643c
diogomartins <diogo.martins@unirio.br>
2015-05-12 15:54:52 -0300

    Adicionada implementação de caracteres uppercase

--- src/pwd/conditions/HasUppercaseLetterCondition.java
+++ src/pwd/conditions/HasUppercaseLetterCondition.java
@@ -3,9 +3,9 @@ package pwd.conditions;
 /**
  * Created by diogomartins on 5/12/15.
  */
-public class HasUppercaseLetterCondition implements PasswordStrengthCondition {
+public class HasUppercaseLetterCondition extends PasswordStrengthCondition {
     @Override
     public int parseString(String password) {
-        return false;
+        return this.countOccurrences(password, UPPER_LETTER);
     }
 }
