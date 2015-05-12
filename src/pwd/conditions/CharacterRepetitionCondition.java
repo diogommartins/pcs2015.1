d2acde0da254e4cae159351b04f1166ec21a6111
diogomartins <diogo.martins@unirio.br>
2015-05-12 17:46:14 -0300

    Adicionada implementação de condição para verificações de repetições na senha

---
+++ src/pwd/conditions/CharacterRepetitionCondition.java
@@ -0,0 +1,31 @@
+package pwd.conditions;
+
+import pwd.StringUtils;
+
+/**
+ * Created by diogomartins on 5/12/15.
+ */
+public class CharacterRepetitionCondition extends PasswordStrengthCondition {
+    @Override
+    public int parseString(String password) {
+        int length = password.length();
+        int total = 0;
+
+        if (length % 2 == 0) { // aaabbb
+            String part1 = password.substring(0, length / 2);
+            String part2 = password.substring(length / 2);
+            if (part1.equals(part2))
+                total++;
+            if (StringUtils.isCharEqual(part1) && StringUtils.isCharEqual(part2))
+                total++;
+        }
+        if (length % 3 == 0) { // ababab
+            String part1 = password.substring(0, length / 3);
+            String part2 = password.substring(length / 3, length / 3 * 2);
+            String part3 = password.substring(length / 3 * 2);
+            if (part1.equals(part2) && part2.equals(part3))
+                total++;
+        }
+        return total;
+    }
+}
