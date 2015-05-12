cd76b79c9b5056f5f7cc9a6f29b42a65b0d6239c
diogomartins <diogo.martins@unirio.br>
2015-05-12 15:51:49 -0300

    Adicionada implementação de analise de tamanho da string da senha

--- src/pwd/conditions/StringSizeCondition.java
+++ src/pwd/conditions/StringSizeCondition.java
@@ -3,7 +3,7 @@ package pwd.conditions;
 /**
  * Created by diogomartins on 5/12/15.
  */
-public class StringSizeCondition implements PasswordStrengthCondition {
+public class StringSizeCondition extends PasswordStrengthCondition {
     @Override
     public int parseString(String password) {
         int total = 0;
@@ -11,9 +11,8 @@ public class StringSizeCondition implements PasswordStrengthCondition {
 
         if (length > 12){
             total++;
-            if (length >= 16){
+            if (length >= 16)
                 total++;
-            }
         }
         return total;
     }
