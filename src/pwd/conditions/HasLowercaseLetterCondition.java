7c25da32aa3c8d111d776910d63bf382c608b2ac
diogomartins <diogo.martins@unirio.br>
2015-05-12 17:22:21 -0300

    Refatoração

--- src/pwd/conditions/HasLowercaseLetterCondition.java
+++ src/pwd/conditions/HasLowercaseLetterCondition.java
@@ -3,12 +3,9 @@ package pwd.conditions;
 /**
  * Created by diogomartins on 5/12/15.
  */
-public class HasLowercaseLetterCondition implements PasswordStrengthCondition {
+public class HasLowercaseLetterCondition extends PasswordStrengthCondition {
     @Override
     public int parseString(String password) {
-        for (Character c: password.toCharArray())
-            if(Character.isLowerCase(c))
-                return true;
-        return false;
+        return this.countOccurrences(password, SMALL_LETTER);
     }
 }
